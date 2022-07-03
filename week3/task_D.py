3ad938be8fe947cc0c880876285bd408315a5821
truEngineer <62888379+truEngineer@users.noreply.github.com>
2022-07-03 21:57:14 +0300

    Update task_D.py

--- week3/task_D.py
+++ week3/task_D.py
@@ -1,9 +1,9 @@
 MAX_LEN = 1e7  # maximum rope length [cm]
-
-
+ 
+ 
 def get_rope_length(lengths, num_of_houses):
-    left = 0  # int(1e-2)
-    right = int(MAX_LEN) + 1  # int(1e7) + 1
+    left = 0
+    right = int(MAX_LEN) + 1
     while right - left > 1:
         mid = (left + right) // 2
         ofv = 0  # objective function value
@@ -14,11 +14,10 @@ def get_rope_length(lengths, num_of_houses):
         else:
             left = mid
     return left
-
-
-n, k = map(int, input().split())  # 4 11
+ 
+ 
+n, k = map(int, input().split())
 ropes = []
 for _ in range(n):
-    #ropes.append([802, 743, 457, 539][i])
-    ropes.append(int(input()))  # [1e-2, 100e3] meters
+    ropes.append(int(input()))
 print(get_rope_length(ropes, k))
