271672d184d2a5ae4e09c0cb04596156d86b8c2f
truEngineer <62888379+truEngineer@users.noreply.github.com>
2022-10-30 14:58:03 +0300

    Update task_A.cpp

--- week14/task_A.cpp
+++ week14/task_A.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <vector>
  
-const int PRIME = 11;
+const int PRIME = 29;
+ 
+std::vector<int> hash;
+std::vector<int> pow;
+ 
+void init(const std::string& s) {
+    pow.resize(s.size() + 1);
+    hash.resize(s.size() + 1);
+    pow[0] = 1;
+    hash[0] = 0;
+    for (int i = 1; i < s.size() + 1; ++i) {
+        pow[i] = PRIME * pow[i - 1];
+        hash[i] = hash[i - 1] * PRIME + s[i - 1];
+    }
+}
+ 
+bool equal(int a, int b, int c, int d) {
+    return (hash[b] - hash[a - 1] * pow[b - (a - 1)] == 
+            hash[d] - hash[c - 1] * pow[d - (c - 1)]);
+}
  
 int main() {
     std::ios_base::sync_with_stdio(false);
@@ -10,21 +29,12 @@ int main() {
  
     std::string s;
     std::cin >> s;
-    std::vector<int> pow(s.size() + 1);
-    std::vector<int> hash(s.size() + 1);
-    pow[0] = 1;
-    hash[0] = 0;
-    for (int i = 1; i < s.size() + 1; ++i) {
-        pow[i] = PRIME * pow[i - 1];
-        hash[i] = hash[i - 1] * PRIME + s[i - 1];
-    }
+    init(s);
     int m, a, b, c, d;
     std::cin >> m;
     for (int i = 0; i < m; ++i) {
         std::cin >> a >> b >> c >> d;
-        std::cout << ((hash[b] - hash[a - 1] * pow[b - (a - 1)] ==
-                       hash[d] - hash[c - 1] * pow[d - (c - 1)]) 
-                       ? "Yes\n" : "No\n");
+        std::cout << (equal(a, b, c, d) ? "Yes\n" : "No\n");
     }
  
     return 0;
