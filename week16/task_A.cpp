2cdc693b7385f0ff4cf244f285795bd413ace1bc
truEngineer <62888379+truEngineer@users.noreply.github.com>
2022-12-06 23:33:27 +0300

    Update task_A.cpp

--- week16/task_A.cpp
+++ week16/task_A.cpp
@@ -1,24 +1,48 @@
 #include <iostream>
+#include <algorithm>
 #include <cmath>
  
-long x1, y1, x2, y2, x3, y3;
+struct point {
+    int x, y;
+ 
+    point() : x(0), y(0) {}
+ 
+    point(int const &x, int const &y) : x(x), y(y) {}
+ 
+    point(point const &other) = default;
+ 
+    int operator/(point const &other) const {
+        return x * other.y - y * other.x;
+    }
+};
+ 
+std::istream &operator>>(std::istream &in, point &p) {
+    return in >> p.x >> p.y;
+}
+ 
+struct segment {
+    point p1, p2;
+ 
+    bool contains(point const &p) const {
+        if ((p.x < p1.x && p.x < p2.x) || (p.y < p1.y && p.y < p2.y))
+            return false;
+        else if ((p.x > p1.x && p.x > p2.x) || (p.y > p1.y && p.y > p2.y))
+            return false;
+        else {
+            double ans = abs((p / p1 + p1 / p2 + p2 / p) / 2.0);
+            return ans == 0;
+        }
+    }
+};
  
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
  
-    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    if( (x1 < x2 && x1 < x3) || (y1 < y2 && y1 < y3))
-        std::cout << "NO";
-    else if( (x1 > x2 && x1 > x3) || (y1 > y2 && y1 > y3))
-        std::cout << "NO";
-    else {
-        double ans = abs(((x1 * y2 - x2 * y1) + 
-                          (x2 * y3 - x3 * y2) + 
-                          (x3 * y1 - x1 * y3)) / 2.0);
-        std::cout << (ans == 0 ? "YES" : "NO");
-    }
+    point p, p1, p2;
+    std::cin >> p >> p1 >> p2;
+    std::cout << (segment{p1, p2}.contains(p) ? "YES\n" : "NO\n");
     
     return 0;
 }
